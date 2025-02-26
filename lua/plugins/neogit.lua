67cd1bd397406e6cf9948f92930d8f2a9c4cbcd9
Tristan Kreuziger <github@tkreuziger.com>
2025-02-26 14:19:49 +0100

    Some better config for neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -2,7 +2,11 @@ return {
     -- An interactive and powerful Git interface for Neovim, inspired by Magit.
     {
         'NeogitOrg/neogit',
-        config = true,
+        opts = {
+            disable_hint = true,
+            process_spinner = true,
+            auto_show_console_on = 'error',
+        },
         enabled = true,
         keys = {
             { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Neogit' },
