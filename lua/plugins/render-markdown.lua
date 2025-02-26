d56d0cf9e4b8d880ca97c06f5fd3d7c27651eb42
Tristan Kreuziger <github@tkreuziger.com>
2025-02-26 14:20:27 +0100

    Enabled pretty Markdown rendering

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+            heading = {
+                sign = false,
+                position = 'inline',
+                width = 'block',
+            },
+        },
+    },
+}
